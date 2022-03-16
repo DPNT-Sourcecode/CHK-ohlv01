add94035d2e43f48e40a79e5bf48dbd4e5611a78
sbx_user1051 <sbx_user1051@169.254.58.193>
2022-03-16 15:30:00 +0000

    Wed Mar 16 15:30:00 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,17 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+# +------+-------+----------------+
+# | Item | Price | Special offers |
+# +------+-------+----------------+
+# | A    | 50    | 3A for 130     |
+# | B    | 30    | 2B for 45      |
+# | C    | 20    |                |
+# | D    | 15    |                |
+# +------+-------+----------------+
+
+
 def checkout(skus):
     raise NotImplementedError()
+
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,8 +1,24 @@
 from solutions.CHK import checkout_solution
 
 
+# +------+-------+----------------+
+# | Item | Price | Special offers |
+# +------+-------+----------------+
+# | A    | 50    | 3A for 130     |
+# | B    | 30    | 2B for 45      |
+# | C    | 20    |                |
+# | D    | 15    |                |
+# +------+-------+----------------+
 
-class TestSum():
 
-    def test_hlo(self):
-        assert checkout_solution.checkout(skus=) ==
+class TestCHK1():
+
+    def test_chk(self):
+        assert checkout_solution.checkout(skus='CDAAABAB') == 20 + 15 + 130 + 50 + 45
+
+    def test_chk1(self):
+        assert checkout_solution.checkout(skus='CDAAABABB') == 20 + 15 + 130 + 50 + 45 + 30
+
+    def test_chk2(self):
+        assert checkout_solution.checkout(skus='CDAAABABBE') == -1
+
