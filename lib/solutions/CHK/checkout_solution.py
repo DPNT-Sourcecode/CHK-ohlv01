273f2c4039325b67c53055f91767d06d88b01713
sbx_user1051 <sbx_user1051@169.254.58.193>
2022-03-16 15:58:00 +0000

    Wed Mar 16 15:58:00 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,15 +13,33 @@
 # | E    | 40    | 2E get one B free      |
 # +------+-------+------------------------+
 
-special_offers = {
-    'A': {
-        'count': 3,
-        'price': 130
-    },
-    'B': {
-        'count': 2,
-        'price': 45
-    }
+special_offers_discount = {
+    'A': [
+        {
+            'count': 3,
+            'price': 130,
+        },
+        {
+            'count': 5,
+            'price': 200,
+        }
+    ],
+    'B': [
+        {
+            'count': 2,
+            'price': 45,
+        }
+    ]
+}
+
+special_offers_free = {
+    'E': [
+        {
+            'count': 2,
+            'target': 'B',
+            'target_count': 1,
+        },
+    ]
 }
 
 prices = {
@@ -29,6 +47,7 @@ prices = {
     'B': 30,
     'C': 20,
     'D': 15,
+    'E': 40,
 }
 
 items = prices.keys()
@@ -55,20 +74,48 @@ def checkout(skus):
     total_price = 0
 
     for purchased_item, count in purchased_items.items():
-        if purchased_item not in special_offers:
+        if purchased_item not in special_offers_discount or purchased_item not in special_offers_free:
             total_price += count * prices[purchased_item]
             continue
 
-        special_offer_count = special_offers[purchased_item]['count']
-        special_offer_price = special_offers[purchased_item]['price']
-
-        special_offers_number = count // special_offer_count
-        regular_purchased_item_number = count % special_offer_count
-
-        total_price += special_offers_number * special_offer_price + regular_purchased_item_number * prices[purchased_item]
-
-    return total_price
+        
 
 
+    # if not skus:
+    #     return 0
+    #
+    # if skus is None:
+    #     return -1
+    #
+    # purchased_items = {}
+    #
+    # for sku in skus:
+    #     if sku not in items:
+    #         return -1
+    #
+    #     if sku not in purchased_items:
+    #         purchased_items[sku] = 0
+    #
+    #     purchased_items[sku] += 1
+    #
+    # total_price = 0
+    #
+    # for purchased_item, count in purchased_items.items():
+    #     if purchased_item not in special_offers:
+    #         total_price += count * prices[purchased_item]
+    #         continue
+    #
+    #     special_offer_count = special_offers[purchased_item]['count']
+    #     special_offer_price = special_offers[purchased_item]['price']
+    #
+    #     special_offers_number = count // special_offer_count
+    #     regular_purchased_item_number = count % special_offer_count
+    #
+    #     total_price += special_offers_number * special_offer_price + regular_purchased_item_number * prices[purchased_item]
+    #
+    # return total_price
+    #
+    #
+    #
 
 
