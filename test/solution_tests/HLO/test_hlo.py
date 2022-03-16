0577e359c1565e25ad0cce3c21d8cc3f64362a27
sbx_user1051 <sbx_user1051@169.254.58.193>
2022-03-16 15:20:57 +0000

    Wed Mar 16 15:20:57 UTC 2022

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -5,4 +5,4 @@ from solutions.HLO import hello_solution
 class TestSum():
 
     def test_hlo(self):
-        assert hello_solution.hello(None) == "Hello World"
+        assert hello_solution.hello(None) == "Hello, World"
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -9,4 +9,3 @@ class TestSum():
         assert sum_solution.compute(1, 2) == 3
     def test_sum_1(self):
         assert sum_solution.compute(2, 2) == 4
-
